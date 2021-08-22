f6f78ebbccb54de2a4c836a3b90d63a689e0ed66
Ricardo Piro-Rael <fdisk87@yahoo.com>
2021-08-22 13:16:44 -0700

    Break dependency on dash
    Also add integration test

--- mastermind-test.el
+++ mastermind-test.el
@@ -6,6 +6,7 @@
 ;;; Code:
 (load-file "./mastermind.el")
 
+;; Testing check-guess function
 (ert-deftest check-guess-when-colors-are-red-red-blue-blue-and-guess-is-blue-blue-red-red ()
   "Colors: red, red, blue blue. Guess: blue, blue, red, red. Should be 4 white hits."
   (should (equal
@@ -22,4 +23,107 @@
   ))
 )
 
+;; Testing remove-first function
+(ert-deftest remove-first-only-no-occurrences ()
+  "Test when the method removes nothing."
+  (should (equal
+    (remove-first 0 (list 1 2 3))
+    (list 3 2 1)
+  ))
+)
+
+(ert-deftest remove-first-only-one-occurrence ()
+  "Test when the method removes one item."
+  (should (equal
+    (remove-first 1 (list 1 2 3))
+    (list 3 2)
+  ))
+)
+
+(ert-deftest remove-first-only-two-occurrences ()
+  "Test when the method removes one item but it appears again."
+  (should (equal
+    (remove-first 1 (list 1 2 3 1))
+    (list 1 3 2)
+  ))
+)
+
+;; Testing elements-in-common function
+(ert-deftest elements-in-common-no-duplicates ()
+  "Check that the common elements of (1, 2, 3) and (2, 3, 4) are (3, 2)"
+  (should (equal
+    (elements-in-common (list 1 2 3) (list 2 3 4))
+    (list 3 2)
+  ))
+)
+
+(ert-deftest elements-in-common-duplicates ()
+  "Check that the common elements of (1, 1, 2, 3) and (1, 1, 2, 2) are (2, 1, 1)"
+  (should (equal
+    (elements-in-common (list 1 1 2 3) (list 1 1 2 2))
+    (list 2 1 1)
+  ))
+)
+
+;; Testing min-by function
+(ert-deftest min-by-empty-list ()
+  "Should be NIL."
+  (should (equal
+    (min-by (lambda (x) (+ 1 x)) (list))
+    nil
+  ))
+)
+
+(ert-deftest min-by-one-element ()
+  "Should be the element."
+  (should (equal
+    (min-by (lambda (x) (+ 1 x)) (list 1))
+    1
+  ))
+)
+
+(ert-deftest min-by-multiple-elements ()
+  "Should be the largest element as we are subtracting it from 1."
+  (should (equal
+    (min-by (lambda (x) (- 1 x)) (list 1 2 3))
+    3
+  ))
+)
+
+;; Testing code-breaker function (integration tests)
+(ert-deftest code-breaker-red-red-blue-blue ()
+  (let ((guess-count 0))
+    (defun code-maker (guess)
+      "Return the red and blue hits for blue blue, red, red and increment guess-count when called."
+      (setq guess-count (+ 1 guess-count))
+      (check-guess '("red" "red" "blue" "blue") guess)
+    )
+    (let ((solution (code-breaker 'code-maker)))
+      (should (equal 2 guess-count))
+      (should (equal '("red" "red" "blue" "blue") solution))
+    )
+  )
+)
+
+(ert-deftest code-breaker-all ()
+  "Test every possibility and see it works on all. WARNING: takes a long time."
+  (let ((possibilities (repeated-permutation '("blue" "red" "white" "green" "black" "yellow") 4)))
+    (let ((solution-guess-counts
+      (mapcar (lambda (possibility)
+        (let ((guess-count 0))
+          (defun code-maker (guess)
+            "A code-maker with POSSIBILITY as the answer."
+            (setq guess-count (+ 1 guess-count))
+            (check-guess possibility guess)
+          )
+          (let ((solution (code-breaker 'code-maker)))
+            (should (equal possibility solution))
+            guess-count
+          )
+        ))
+      possibilities)))
+      (should (equal 5 (apply #'max solution-guess-counts)))
+    )
+  )
+)
 ;;; mastermind-test.el ends here
--- mastermind.el
+++ mastermind.el
@@ -1,6 +1,8 @@
-;;; Mastermind --- solves mastermind
-;;; Commentary: First makes a guess then takes back peg colors and counts as input.
-
+;;; mastermind.el --- solves mastermind
+;;; Commentary:
+;;
+;; Description:
+;;
 ;;; Code:
 (defun repeated-permutation (list size) "Return the permutations of LIST with SIZE." (
     if (or (null list) (= 0 size)) '(()) (
@@ -16,107 +18,154 @@
   )
 ))
 
-(defun check-guess (code guess)
-  "Return pegs for an individual GUESS against a CODE."
-  (setq pairs (-zip-lists code guess))
-  (setq non-red-hits (seq-remove (lambda (color) (apply #'string= color)) pairs))
-  (setq red-hits-count (- (length code) (length non-red-hits)))
-  (setq unchecked-code-elements (mapcar (lambda(pair) (car pair)) non-red-hits))
-  (setq unchecked-guess-elements (mapcar (lambda(single) (car (last single))) non-red-hits))
-  (setq white-hits-count 0)
-  (dolist (color unchecked-guess-elements white-hits-count)
-    (if (member color unchecked-code-elements)
-      (progn
-        (setq unchecked-code-elements (--remove-first (string= it color) unchecked-code-elements))
-        (setq white-hits-count (+ 1 white-hits-count))
+(defun remove-first (element-to-remove original-list)
+  "Remove the first occurence of ELEMENT-TO-REMOVE from ORIGINAL-LIST. Note this doesn't preserve order."
+  (let ((list-with-element-removed) (element-has-been-found nil))
+    (dolist (element original-list list-with-element-removed)
+      (if (and (not element-has-been-found) (equal element element-to-remove))
+        (setq element-has-been-found t)
+        (setq list-with-element-removed (cons element list-with-element-removed))
       )
-      nil
     )
   )
-  (list red-hits-count white-hits-count)
+)
+
+(defun min-by (mapping-function list)
+  "Return the minimum element of LIST based on MAPPING-FUNCTION."
+  (if (not list) nil
+
+    (let* ((min-element (car list)) (min-value (funcall mapping-function (car list))) (element))
+      (dolist (element (cdr list) min-element)
+        (let ((mapped-element (funcall mapping-function element)))
+          (if (string< mapped-element min-value)
+            (progn (setq min-element element) (setq min-value mapped-element))
+            nil
+          )
+        )
+      )
+      min-element
+    )
+  )
+)
+
+(defun elements-in-common (first-list second-list)
+  "Get elements in common between FIRST-LIST and SECOND-LIST, counting duplicates."
+  (let (common-elements)
+    (dolist (element first-list common-elements)
+        (if (member element second-list)
+          (progn
+            (setq second-list (remove-first element second-list))
+            (setq common-elements (cons element common-elements))
+          )
+          nil
+        )
+    )
+  )
+)
+
+(defun check-guess (code guess)
+  "Return pegs for an individual GUESS against a CODE."
+  (let* (
+      (pairs (-zip-lists code guess))
+      (non-red-hits (seq-remove (lambda (color) (apply #'string= color)) pairs))
+      (red-hits-count (- (length code) (length non-red-hits)))
+      (unchecked-code-elements (mapcar (lambda(pair) (car pair)) non-red-hits))
+      (unchecked-guess-elements (mapcar (lambda(single) (car (last single))) non-red-hits))
+      (white-hits-count (length (elements-in-common unchecked-code-elements unchecked-guess-elements)))
+    )
+    (list red-hits-count white-hits-count)
+  )
 )
 
 (defun code-breaker (codemaker-check-guess) "Breaks a mastermind code with feedback from CODEMAKER-CHECK-GUESS."
-  (setq colors '("blue" "red" "white" "green" "black" "yellow"))
-  (defun enumerate-possibilities () "All possible guess." (repeated-permutation colors 4))
-  (setq initial-guess '("blue" "blue" "red" "red"))
-  (setq guess initial-guess)
-  (setq guesses (make-hash-table :test 'equal))
-  (setq all-possibilities (enumerate-possibilities))
-  (setq possibilities (enumerate-possibilities))
+  (let* (
+      (colors '("blue" "red" "white" "green" "black" "yellow"))
+      (initial-guess '("blue" "blue" "red" "red"))
+      (guess initial-guess)
+      (guesses (make-hash-table :test 'equal))
+      (all-possibilities (repeated-permutation colors 4))
+      (possibilities (repeated-permutation colors 4))
+    )
 
-  (defun break-code! ()
-    (with-output-to-string (princ "Guessing ") (princ guess))
-    (make-guess)
-    (if (= 4 (car (gethash guess guesses))) guess
-      (progn
-        (setq possibilities (calculate-remaining-possibilities guess (gethash guess guesses)))
-        (setq guess (find-best-guess))
-      nil
+    (defun break-code! ()
+      (with-output-to-string (princ "Guessing ") (princ guess))
+      (make-guess)
+      (if (= 4 (car (gethash guess guesses))) guess
+        (progn
+          (setq possibilities (calculate-remaining-possibilities guess (gethash guess guesses)))
+          (setq guess (find-best-guess))
+        nil
+        )
       )
     )
-  )
 
-  (defun make-guess () (puthash guess (funcall codemaker-check-guess guess) guesses))
+    (defun make-guess () (puthash guess (funcall codemaker-check-guess guess) guesses))
 
-  (defun calculate-remaining-possibilities (guess result)
-    "Enumerate possibilities that remain after the RESULT of the current GUESS."
-    (seq-filter (lambda (possibility) (equal result (check-guess guess possibility))) (remove guess possibilities))
-  )
+    (defun calculate-remaining-possibilities (guess result)
+      "Enumerate possibilities that remain after the RESULT of the current GUESS."
+      (seq-filter (lambda (possibility) (equal result (check-guess guess possibility))) (remove guess possibilities))
+    )
 
-  (defun find-best-guess ()
-    (if (<= (length possibilities) 2) (car possibilities) (progn
+    (defun find-best-guess ()
+      (if (<= (length possibilities) 2) (car possibilities) (progn
 
-      (setq unguessed-possibilities (seq-remove (lambda (possibility) (gethash possibility guesses)) all-possibilities))
-      (setq all-guesses
-        (mapcar (
-          lambda (possible-guess) (progn
-            (setq results-with-counts (make-hash-table :test 'equal))
-            (mapc (
-                lambda (possibility) (progn
-                  (setq result (check-guess possible-guess possibility))
-                  (puthash result (+ 1 (gethash result results-with-counts 0)) results-with-counts)
-                ))
-            possibilities)
-            (setq highest-count (
-              apply #'max (hash-table-values results-with-counts)
+        (let* (
+            (unguessed-possibilities (seq-remove (lambda (possibility) (gethash possibility guesses)) all-possibilities))
+            (all-guesses
+              (mapcar (
+                lambda (possible-guess) (progn
+                  (let ((results-with-counts (make-hash-table :test 'equal)))
+                    (dolist (possibility possibilities)
+                      (let ((result (check-guess possible-guess possibility)))
+                        (puthash result (+ 1 (gethash result results-with-counts 0)) results-with-counts)))
+                    (let ((highest-count (apply #'max (hash-table-values results-with-counts))))
+                      (list possible-guess highest-count))
+                )))
+                unguessed-possibilities)
+              )
+            (worst-case-smallest-group (
+              apply #'min (mapcar (lambda (guess-and-count) (car (last guess-and-count))) all-guesses)
             ))
-            (list possible-guess highest-count)
-          ))
-          unguessed-possibilities)
-      )
-      (setq worst-case-smallest-group (
-        apply #'min (mapcar (lambda (guess-and-count) (car (last guess-and-count))) all-guesses)
-      ))
-      (setq best-guesses (
-        mapcar #'car (
-          seq-filter (lambda(guess-with-count) (= worst-case-smallest-group (car (last guess-with-count)))) all-guesses
+            (best-guesses (
+              mapcar #'car (
+                seq-filter (lambda(guess-with-count) (= worst-case-smallest-group (car (last guess-with-count)))) all-guesses
+              )
+            ))
+          )
+          (min-by (
+              lambda (good-guess) (concat (if (member good-guess possibilities) "a" "b") (string-join good-guess)))
+            best-guesses
+          )
         )
       ))
-      (--min-by (
-          lambda (good-guess) (concat (if (member good-guess possibilities) "a" "b") (string-join good-guess)))
-        best-guesses
+    )
+    (let (
+        (solution nil)
+        (number-of-guesses 0)
       )
-    ))
+      (while (eq nil solution)
+        (setq solution (break-code!))
+        (setq number-of-guesses (+ 1 number-of-guesses))
+      )
+      (with-output-to-string (princ "Guesses ") (princ number-of-guesses))
+      solution
+    )
   )
-  (setq solution nil)
-  (setq number-of-guesses 0)
-  (while (eq nil solution)
-    (setq solution (break-code!))
-    (setq number-of-guesses (+ 1 number-of-guesses))
-  )
-  (with-output-to-string (princ "Guesses ") (princ number-of-guesses))
 )
 
 (defun human-code-maker (guess)
   "Prints the GUESS out and has the human enter the hit counts."
-  (setq red-hits (read-number (concat (string-join guess ", ") " Red hits: ")))
-  (setq white-hits (read-number (concat (string-join guess ", ") " White hits: ")))
-  (list red-hits white-hits)
+  (let (
+      (red-hits (read-number (concat (string-join guess ", ") " Red hits: ")))
+      (white-hits (read-number (concat (string-join guess ", ") " White hits: ")))
+    )
+    (list red-hits white-hits)
+  )
 )
 
 ;;;###autoload
 (defun mastermind ()
+  "Will guess a mastermind code by using feedback a human gives."
   (interactive)
   (code-breaker 'human-code-maker))
 
